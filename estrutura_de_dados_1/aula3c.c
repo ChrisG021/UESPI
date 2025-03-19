381c5acccfdb2868b9fb11764924670c1d7fd523
chris <christopheguerra67@gmail.com>
2025-03-19 20:20:59 -0300

    Alteração na tarefa 3c

--- estrutura_de_dados_1/aula3c.c
+++ estrutura_de_dados_1/aula3c.c
@@ -1,29 +1,30 @@
-#include<stdio.h>
+#include <stdio.h>
 
-int main(void){
-    int n[]={5, 12, 23, 34, 45, 56, 67, 78, 89, 90, 
-        21, 32, 43, 54, 65, 76, 87, 98, 11, 22};
-    int i;
-    int mai,men;
-    float ave;
-    mai= men = n[0];
+int main(void) {
+    int n[] = {5, 12, 23, 34, 45, 56, 67, 78, 89, 90, 
+               21, 32, 43, 54, 65, 76, 87, 98, 11, 22};
+    int i = 0;
+    int mai, men;
+    float ave = 0;  // Inicializando a variável
 
-    for(i = 0; i<20; i++){
-        /*Fun*/
-        if (n[i]> mai){
+    mai = men = n[0];
+
+    do {
+        if (n[i] > mai) {
             mai = n[i];
         }
-        if(n[i]<men){
+        if (n[i] < men) {
             men = n[i];
         }
-        ave +=n[i];
-    } 
+        ave += n[i];
+        i++;
+    } while (i < 20);  // Corrigido para 20 elementos
 
-    printf("maior numero: %d",mai);
-    printf("\nmenor numero: %d",men);
+    ave = ave / 20; // Calcular a média corretamente
 
-    ave = ave/20;
-    printf("\nMedia: %d",ave);
+    printf("Maior numero: %d\n", mai);
+    printf("Menor numero: %d\n", men);
+    printf("Media: %.2f\n", ave);  // %.2f para exibir duas casas decimais
 
     return 0;
 }
